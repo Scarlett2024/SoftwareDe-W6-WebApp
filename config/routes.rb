456b14a3508006f63228c9cfb15241eda1665cac
Scarlett Ma <yiyun.ma@kellogg.northwestern.edu>
2024-02-12 00:16:33 +0000

    completed class content

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/tacos", :controller => "tacos", :action => "index"
   get "/dice", :controller => "dice", :action => "index"
   get "/cards", :controller => "cards", :action => "index"
+  resources "companies"
+  # equal to get "/companies", :controller => "companies", :action => "index"
 end
